5d40c1595c354e6e19bdac0690bdad2e92d33d00
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-29 17:24:10 +0300

    second commit

--- src/main/java/ru/ibs/StarterClass.java
+++ src/main/java/ru/ibs/StarterClass.java
@@ -1,2 +1,11 @@
-package ru.ibs;public class StarterClass {
+package ru.ibs;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StarterClass {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(StringUtils.upperCase(args[i]));
+        }
+    }
 }
