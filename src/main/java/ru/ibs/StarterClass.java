96187f4739aefa6e8c4cdb3594d6617a5a61fcc4
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-29 17:44:34 +0300

    Верхний регистр получился,но без АПАЧИ!

--- src/main/java/ru/ibs/StarterClass.java
+++ src/main/java/ru/ibs/StarterClass.java
@@ -2,10 +2,22 @@ package ru.ibs;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
+
 public class StarterClass {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
-            System.out.println(StringUtils.upperCase(args[i]));
+            String str = args[i];
+           // String str2 = StringUtils.upperCase(str);
+            //show(str2);
+            showWithoutApache(str);
         }
+    }
+    public static void show(String str){
+        System.out.println(String.format(str));
+    }
+
+    public static void showWithoutApache(String str){
+        System.out.println(str.toUpperCase());
     }
 }
